9a91d9c59658ca93fa4beed4e08f015b87a439af
mesakh123 <cmesakh@ymail.com>
2023-04-27 20:49:45 +0800

    fix(python): change to sync

--- pressure_sore_with_admin/settings.py
+++ pressure_sore_with_admin/settings.py
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     'django_cookies_samesite.middleware.CookiesSameSite',
 ]
 
-BACKGROUND_TASK_RUN_ASYNC = True
+BACKGROUND_TASK_RUN_ASYNC = False
 SESSION_COOKIE_SAMESITE = 'strict'
 ROOT_URLCONF = 'pressure_sore_with_admin.urls'
 TEMPLATES = [
